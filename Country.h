d862c60bc896e315b6eb8d8240306454576dcb25
Miles00730 <kuhnmiles7@gmail.com>
2016-02-06 16:16:21 -0600

    Create Country.h

---
+++ Country.h
@@ -0,0 +1,43 @@
+#ifndef COUNTRY_H
+#define COUNTRY_H
+
+#include <SFML/Graphics>
+#include <iostream>
+#include <string>
+
+class Country {
+    public:
+        Country(string countryName);
+        
+        string name;
+        
+        int getSoybeans();
+        int getUranium();
+        int getWater();
+        int getubber();
+        int getNaturalGas();
+        int getGold();
+        int getCotton();
+        int getSilver();
+        int getCorn();
+        int getOil();
+        int getRice();
+        int getDiamonds();
+        
+    private:
+        int soybeans;
+        int uranium;
+        int wheat;
+        int water;
+        int rubber;
+        int naturalGas;
+        int gold;
+        int cotton;
+        int silver;
+        int corn;
+        int oil;
+        int rice;
+        int diamonds;
+};
+
+#endif
