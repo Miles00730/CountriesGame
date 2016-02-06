7449929a56d2b99e50d691787b1d3824040b8f95
Miles00730 <kuhnmiles7@gmail.com>
2016-02-06 16:16:27 -0600

    Create Country.cpp

---
+++ Country.cpp
@@ -0,0 +1,43 @@
+#include "Country.h"
+
+Country::Country(int countryName) {
+    name = countryName;
+}
+
+int Country::getSoybeans() {
+    return soybeans;
+}
+
+int Country::getUranium() {
+    return uranium;
+}
+int Country::getWater() {
+    return water;
+}
+int Country::getRubber() {
+    return rubber;
+}
+int Country::getNaturalGas() {
+    return naturalGas;
+}
+int Country::getGold() {
+    return gold;
+}
+int Country::getCotton() {
+    return cotton;
+}
+int Country::getSilver() {
+    return silver;
+}
+int Country::getCorn() {
+    return corn;
+}
+int Country::getOil() {
+    return oil;
+}
+int Country::getRice() {
+    return rice;
+}
+int Country::getDiamonds() {
+    return diamonds;
+}
