3bad65e83c3cf810a8d4771f9d1972d04f056cea
Miles00730 <kuhnmiles7@gmail.com>
2016-02-11 17:05:34 -0600

    Update Country.h

--- Country.h
+++ Country.h
@@ -24,7 +24,7 @@ class Country {
         int getRice();
         int getDiamonds();
         int getPopulation(); 
-        
+        Country(int, int, int, int, int, int, int, int, int,int, int, int, int, string);  
     private:
         int soybeans;
         int uranium;
@@ -41,5 +41,33 @@ class Country {
         int diamonds;
         int Population
 };
+void Country::Country(  int soybeans, int uranium,int wheat,int water,int rubber,int naturalGas,int gold,int cotton,int silver
+,int corn,int oil,int rice, int diamonds, int Population, string name) { 
+   this -> soybeans = soybeans; 
+   this -> uranium = uranium;
+   this -> wheat = wheat;
+   this -> water = water;
+   this -> rubber = rubber;
+   this -> naturalGas = naturalGas;
+  this -> gold = gold;
+  this -> cotton = cotton;
+  this -> silver = silver;
+  this -> corn = corn;
+  this -> oil = oil;
+   this -> rice = rice;
+  this -> diamonds = diamonds;
+   this -> Population = Population;
+    this -> name = name;
+  
+  
+    
+} 
+int main() {
+  Country France(// pass values from constructor Countries 
+  );
+  Country UnitedStates(); 
+  
+    
+} 
 
 #endif
